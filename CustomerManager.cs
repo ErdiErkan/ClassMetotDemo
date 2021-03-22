e2e1ea26eb7f51a3602dcdd9d9b4fb26ed778b43
erdi1 <erdierkan1@gmail.com>
2021-03-23 00:29:01 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekle(Customers customer)
+        {
+            Console.WriteLine("Tebrikler Müşteri Ad Ekleme İşmeniniz Gerçekleşti : " + customer.Ad);
+        }
+
+        public void Sil(Customers customer)
+        {
+            Console.WriteLine("Tebrikler Müşteri TcNo Silme İşmeniniz Gerçekleşti : " + customer.TcNo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customers customer1 = new Customers();
+            customer1.Ad = "Mert";
+            customer1.Soyad = "Enkan";
+            customer1.TcNo = "1111111111";
+            customer1.DogumYeri = "Bakırköy";
+
+            Customers customer2 = new Customers();
+            customer2.Ad = "Sinan";
+            customer2.Soyad = "Bozok";
+            customer2.TcNo = "2222222222";
+            customer2.DogumYeri = "Adana";
+
+            Customers customer3 = new Customers();
+            customer3.Ad = "Pelin";
+            customer3.Soyad = "Sertac";
+            customer3.TcNo = "3333333333";
+            customer3.DogumYeri = "Kars";
+
+            Customers customer4 = new Customers();
+            customer4.Ad = "Fahri";
+            customer4.Soyad = "Gunok";
+            customer4.TcNo = "4444444444";
+            customer4.DogumYeri = "Kastamonu";
+
+            Customers[] customers = new Customers[]
+            {
+                customer1,customer2,customer3,customer4
+            };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekle(customer1);
+            customerManager.Sil(customer3);
+
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var custom in customers)
+            {
+                Console.WriteLine(custom.Ad);
+            }
+            Console.WriteLine("Müşteriler Başarılı Şekilde Sıralandı");
+        }
+    }
+}
